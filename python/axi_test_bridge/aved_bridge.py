4b57307ebbd2882f48fe51d8c8b5443e6a794439
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2026-09-12 19:42:13 -0700

    changed FPGA init msg

--- python/axi_test_bridge/aved_bridge.py
+++ python/axi_test_bridge/aved_bridge.py
@@ -45,7 +45,7 @@ class AVED_Bridge:
         handler.setFormatter(logging.Formatter("%(message)s"))
         self.log.addHandler(handler)
 
-        self.log.info("FPGA Emulation Initialized!\n")
+        self.log.info("FPGA Simulation Environment Initialized!\n")
 
     def writelog(self, txt):
         print(txt, end='')
